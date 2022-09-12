7e41243e7e353f5b22ffbca3cad53c3206731663
DundeShini <112486766+DundeShini@users.noreply.github.com>
2022-09-12 09:29:46 +0530

    Update leap year.py

--- leap year.py
+++ leap year.py
@@ -1,7 +1,5 @@
 # Python program to check if year is a leap year or not
-
-year = 2000
-# year = int(input("Enter a year: "))
+year = int(input("Enter a year: "))
 if (year % 400 == 0) and (year % 100 == 0):
     print("{0} is a leap year".format(year))
 elif (year % 4 ==0) and (year % 100 != 0):
