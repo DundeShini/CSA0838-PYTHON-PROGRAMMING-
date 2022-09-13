f017086f7f3dc6eac5d4687a6d49d9abb54adbda
DundeShini <112486766+DundeShini@users.noreply.github.com>
2022-09-13 18:15:33 +0530

    Update marks and average.py

--- marks and average.py
+++ marks and average.py
@@ -7,13 +7,13 @@ s5=int(input("enter the marks of fifth subject"))
 avg= (s1+s2+s3+s4+s5)/5
 if (avg>=90):
     print("grade:S")
-elif (avg<=90)&(avg>=80):
+elif (avg<90)&(avg>=80):
     print("grade:A")
-elif (avg<=80)&(avg>=70):
+elif (avg<80)&(avg>=70):
     print("grade:B")
-elif (avg<=70)&(avg>=60):
+elif (avg<70)&(avg>=60):
     print("grade:C")
-elif (avg<=60)&(avg>=50):
+elif (avg<60)&(avg>=50):
     print("grade:D")
 else:
     print("grade:F")
