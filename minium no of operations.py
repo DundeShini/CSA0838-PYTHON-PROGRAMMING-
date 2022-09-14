11daec8ac0a0022602b2bc6a807efdce6c36fb7a
DundeShini <112486766+DundeShini@users.noreply.github.com>
2022-09-14 09:38:37 +0530

    Add files via upload

---
+++ minium no of operations.py
@@ -0,0 +1,10 @@
+class Solution:
+   def solve(self, s, t):
+      k, n = len(t), len(s)
+      ans = 10**10
+      for i in range(n - k + 1):
+         ss = s[i:i+k]
+         ans = min(ans, sum(ss[j]!=t[j] for j in range(k)))
+      return ans
+ob = Solution()
+print(ob.solve(input("s1:"), input("s2:")))
