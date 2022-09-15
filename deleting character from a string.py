eaf56f3fb7cbd71de4c8e01a1e715a712214fca1
DundeShini <112486766+DundeShini@users.noreply.github.com>
2022-09-15 09:05:29 +0530

    Update deleting character from a string.py

--- deleting character from a string.py
+++ deleting character from a string.py
@@ -1,11 +1,10 @@
-input_string = input("the input string is:")
-char_to_remove = input("the character to delete is:")
-newStr = ""
-for character in input_string:
-    if character != char_to_remove:
-        newStr += character
-
-print("The input string is:", input_string)
-print("The character to delete is:", char_to_remove)
-print("The output string is:", newStr)
-
+def delchar(s,c):
+    if len(c)==1:
+        for i in range(len(s)):
+            if c not in s[i]:
+                print(s[i])
+            else:
+                continue
+string= input("enter the string = ")
+char = input("enter the character to be deleted = ")
+delchar(string,char)
