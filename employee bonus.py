d487cb014cb902076858139b8bee319c886ffb21
DundeShini <112486766+DundeShini@users.noreply.github.com>
2022-09-15 11:44:03 +0530

    Update employee bonus.py

--- employee bonus.py
+++ employee bonus.py
@@ -2,7 +2,7 @@ grade=input("enter the grade(A/B) :")
 salary= int(input("enter the salary :"))
 if (grade == 'A') and (salary>10000):
     bonus= salary*(5/100)
-    print(" total salary is:", bonus)
+    print(" total salary is:", bonus+salary)
 elif (grade == 'B') and (salary>10000):
     bonus= salary*(10/100)
     print("total salary is:",bonus+salary)
